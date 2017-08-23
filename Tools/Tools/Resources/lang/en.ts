36250931b022f83bd47e325744b7e0ff7c9c3cd3
liam <rainpatrol@live.com>
2017-08-23 09:31:28 +0800

    resources.

---
+++ Tools/Tools/Resources/lang/en.ts
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Tools</name>
+    <message>
+        <location filename="Tools.ui" line="14"/>
+        <source>Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Tools.cpp" line="30"/>
+        <location filename="Tools.cpp" line="92"/>
+        <source>Column 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Tools.cpp" line="30"/>
+        <location filename="Tools.cpp" line="92"/>
+        <source>Column 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
